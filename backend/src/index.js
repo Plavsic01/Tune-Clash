37e4584a5bfa774b15f4f6bbe1581aae71e3f2a0
Andrej Plavsic <andrejplavsic3@gmail.com>
2024-12-02 17:28:09 +0100

    Changes for deployment on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-import dotenv from "dotenv/config";
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
+dotenv.config({ path: "./backend/.env" });
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,8 +18,13 @@ app.use(
 );
 
 app.use("/audio", express.static(path.join(__dirname, "audio")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode`);
-  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log(`Listening on port:${PORT}`);
 });
